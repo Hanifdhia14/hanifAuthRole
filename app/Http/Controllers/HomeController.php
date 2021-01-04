80ed03e89f5e284489f82f574d2cacd059ee25fc
SandyMaull <sandymaulana0@gmail.com>
2021-01-04 20:50:48 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        // Role Admin
+        $this->middleware('role')->only(['testAdmin']);
+        // Role Selain Admin, bisa Custom, tinggal edit $this->middleware('role'), sperti dibawah sesuai dgn role di DB
+        // pada dasarnya role Admin bisa akses kesemua function yg ada di controller, tp kalau mau rubah, bisa di file Middleware->Role.php
+        $this->middleware('role:Manager')->only(['testManager']);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function testAdmin()
+    {
+        return "Page Admin Only!";
+    }
+
+    public function testManager()
+    {
+        return "Test Manager Page!";
+    }
+}
