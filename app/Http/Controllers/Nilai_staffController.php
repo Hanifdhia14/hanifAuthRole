e06033a4a492a9040b4f250d3c243a7203e94206
hanif <hanif.dhiaeartha14@gmail.com>
2021-07-29 09:17:11 +0800

    penambahan video openeing

--- app/Http/Controllers/Nilai_staffController.php
+++ app/Http/Controllers/Nilai_staffController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class Nilai_staffController extends Controller
 {
     public function index()
-    {return view('leader.nilai_staff.index')
+    {
+        return view('leader.nilai_staff.index');
     }
 }
